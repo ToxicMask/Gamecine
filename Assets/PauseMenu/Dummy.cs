7f881dfbe5b1a711914c9f8b684733904093eb31
ToxicMask <tomas.carciofi@gmail.com>
2020-04-14 14:53:31 -0300

    Dummy Test: Code

---
+++ Assets/PauseMenu/Dummy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PauseTest
+{
+    public class Dummy : MonoBehaviour
+    {
+
+        [SerializeField] bool rotate = false;
+        [SerializeField] float rotationSpeed = 2.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (rotate)
+            {
+                transform.Rotate(Vector3.forward * rotationSpeed);
+            }
+
+        }
+    }
+}
