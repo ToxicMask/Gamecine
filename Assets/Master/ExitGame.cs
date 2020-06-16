04812060f767097844458418024224050b0cf78f
ToxicMask <tomas.carciofi@gmail.com>
2020-06-16 05:02:29 -0300

    Build pre-alpha 0.0.1

---
+++ Assets/Master/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A code to close the aplication in the scene
+
+
+public class ExitGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
