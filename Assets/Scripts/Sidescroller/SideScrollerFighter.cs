bd67d80c372961ee2304d7d24b49c7930ffae486
ToxicMask <tomas.carciofi@gmail.com>
2020-09-15 03:34:54 -0300

    Audio Pipeline - SFX PLACEHOLDERS

--- Assets/Scripts/Sidescroller/SideScrollerFighter.cs
+++ Assets/Scripts/Sidescroller/SideScrollerFighter.cs
@@ -25,6 +25,14 @@ namespace Sidescroller.Fighting
 
         public Transform attackPos;
 
+        public AudioListener audioListener;
+
+        public AudioClip audioDamaged;
+        public AudioClip audioAttackGrunt;
+        public AudioClip audioAttackHit;
+        public AudioClip audioAttackBlock;
+        public AudioClip audioDeath;
+
         private Vector3 startPosition;
 
 
@@ -49,7 +57,6 @@ namespace Sidescroller.Fighting
         {
             animator.SetFloat("Walk", 0f);
             animator.SetTrigger("Reset");
-
         }
 
         public void AttackBasic()
@@ -71,6 +78,7 @@ namespace Sidescroller.Fighting
         {
             currentState = FighterState.Damaged;
             animator.SetTrigger("Damage");
+            if (audioDamaged != null) AudioSource.PlayClipAtPoint( audioDamaged, audioListener.transform.position, 1f);
         }
 
         public void Death()
@@ -79,11 +87,16 @@ namespace Sidescroller.Fighting
             currentState = FighterState.Dying;
             animator.SetTrigger("Death");
 
+            if (audioDeath != null) AudioSource.PlayClipAtPoint(audioDeath, audioListener.transform.position, 1f);
         }
-        
+
         private void AttackBasicHit()
         {
             Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);//todo mudar pra box quando tiver as sprites certas
+
+            // Battle Cry
+            if ( audioAttackGrunt != null) { AudioSource.PlayClipAtPoint(audioAttackGrunt, audioListener.transform.position, .6f);}
+
             foreach (Collider2D collider in enemiesToDamage)
             {
                 if (collider == this.gameObject.GetComponent<Collider2D>()) continue;
@@ -93,15 +106,20 @@ namespace Sidescroller.Fighting
                     {
                         if (collider.GetComponent<SideScrollerFighter>().currentState == FighterState.Blocking)
                         {
-                            this.gameObject.GetComponent<Health>().TakeDamage(1f);
+                            // Attacked Blocked
+                            if (audioAttackBlock != null) AudioSource.PlayClipAtPoint(audioAttackBlock, audioListener.transform.position, .8f);
+                            if (audioDamaged != null) AudioSource.PlayClipAtPoint(audioDamaged, audioListener.transform.position, .6f);
+                            this.gameObject.GetComponent<Health>().TakeDamage(1f); // Block Damage
                             continue;
                         }
 
                     }
-
+                    // Hit Player
+                    if (audioAttackHit != null) AudioSource.PlayClipAtPoint(audioAttackHit, audioListener.transform.position, .2f);
                     collider.GetComponent<Health>().TakeDamage(attackDamage);
                 }
             }
+
         }
 
 
