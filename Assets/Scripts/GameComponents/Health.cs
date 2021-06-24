f6ca78f1633586fa2c655530e9bf4f1fd1079949
bigassopper <deathdiec@gmail.com>
2021-06-23 21:22:33 -0300

    updatezinha rapida + setup das coisas

---
+++ Assets/Scripts/GameComponents/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int health;
+    public int maxHealth;
+    public int Current{
+        get{return health;}
+        set{
+            health = value;
+            if(IsDead()){
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    public bool IsDead(){
+        if(health <= 0) return true;
+        return false;
+    }
+    private void Start() {
+        health = maxHealth;
+    }
+}
---
+++ Assets/Scripts/GameComponents/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Timer
+{
+    
+    public float elapsed;
+    public float setTime;
+
+    public event Action OnComplete = delegate { };
+
+    public bool complete {
+        get { return elapsed >= setTime; }
+    }
+
+    public Timer(float _set) {
+        setTime = _set;
+    }
+
+    public void Update() {
+        elapsed += Time.deltaTime;
+
+        if (elapsed >= setTime) {
+            OnComplete();
+        }
+        
+    }
+
+    public void Reset() {
+        elapsed = 0;
+    }
+}
