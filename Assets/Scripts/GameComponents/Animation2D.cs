a353e4311500a368f313856a73de999b6bcd3e9e
ToxicMask <tomas.carciofi@gmail.com>
2021-07-21 16:07:37 -0300

    New Animation 2D Script

---
+++ Assets/Scripts/GameComponents/Animation2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameComponents
+{
+    public class Animation2D : MonoBehaviour
+    {
+
+        Animator animator = null;
+
+        string currentState = "";
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update Animation State
+        void PlayClip(string newState)
+        {
+            // Check Current State
+            if (currentState == newState) return;
+
+            // Switch State
+            if (animator) animator.Play(newState);
+            else print("NO ANIMATOR");
+
+            // Update Animation Script
+            currentState = newState;
+        }
+
+
+        float GetStateLenght()
+        {
+            return animator.GetCurrentAnimatorStateInfo(0).length;
+        }
+        
+    }
+}
