cc69a36439028885c3e3831ccf6190eab92df3ab
bigassopper <deathdiec@gmail.com>
2021-09-04 09:44:32 -0300

    chicken cutscene

---
+++ Assets/Scripts/ChickenMinigame/Animation/DestroyBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBehaviour : StateMachineBehaviour
+{
+    public override void  OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        Destroy(animator.gameObject, animatorStateInfo.length);
+    }
+}
